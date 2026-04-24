a210fd1bc5d9b879f64219d8f026c656d961b5a9
Acid3croco <yoann.ditte@gmail.com>
2026-04-24 23:21:07 +0000

    Migrate LSP setup to vim.lsp.config / mason-lspconfig 2.x

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,67 +1,58 @@
 return {
-  { "williamboman/mason.nvim", cmd = "Mason", opts = {} },
+  { "williamboman/mason.nvim", opts = {} },
 
   {
     "williamboman/mason-lspconfig.nvim",
-    version = "^1.0",
-    dependencies = { "williamboman/mason.nvim" },
-    opts = {
-      ensure_installed = { "lua_ls" },
-      automatic_installation = true,
-    },
-  },
-
-  {
-    "neovim/nvim-lspconfig",
     event = { "BufReadPre", "BufNewFile" },
     dependencies = {
-      "williamboman/mason-lspconfig.nvim",
+      "williamboman/mason.nvim",
+      "neovim/nvim-lspconfig",
       "saghen/blink.cmp",
     },
     config = function()
-      local lsp  = require("lspconfig")
       local caps = vim.lsp.protocol.make_client_capabilities()
       pcall(function()
         caps = require("blink.cmp").get_lsp_capabilities(caps)
       end)
 
-      local on_attach = function(_, bufnr)
-        local function k(mode, l, r, desc)
-          vim.keymap.set(mode, l, r, { buffer = bufnr, desc = desc })
-        end
-        k("n", "gd", vim.lsp.buf.definition,     "Definition")
-        k("n", "gr", vim.lsp.buf.references,     "References")
-        k("n", "gi", vim.lsp.buf.implementation, "Implementation")
-        k("n", "K",  vim.lsp.buf.hover,          "Hover")
-        k("n", "<leader>cr", vim.lsp.buf.rename,      "Rename")
-        k("n", "<leader>ca", vim.lsp.buf.code_action, "Code action")
-        k("n", "<leader>cf", function() vim.lsp.buf.format({ async = true }) end, "Format")
-        k("n", "]d", vim.diagnostic.goto_next, "Next diagnostic")
-        k("n", "[d", vim.diagnostic.goto_prev, "Prev diagnostic")
-      end
+      vim.lsp.config("*", { capabilities = caps })
 
-      local servers = {
-        lua_ls = {
-          settings = {
-            Lua = {
-              workspace   = { checkThirdParty = false },
-              telemetry   = { enable = false },
-              diagnostics = { globals = { "vim" } },
-            },
+      vim.lsp.config("lua_ls", {
+        settings = {
+          Lua = {
+            workspace   = { checkThirdParty = false },
+            telemetry   = { enable = false },
+            diagnostics = { globals = { "vim" } },
           },
         },
-      }
+      })
 
-      for name, cfg in pairs(servers) do
-        cfg.capabilities = caps
-        cfg.on_attach    = on_attach
-        lsp[name].setup(cfg)
-      end
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls" },
+      })
+
+      vim.api.nvim_create_autocmd("LspAttach", {
+        callback = function(args)
+          local bufnr = args.buf
+          local function k(mode, l, r, desc)
+            vim.keymap.set(mode, l, r, { buffer = bufnr, desc = desc })
+          end
+          k("n", "gd", vim.lsp.buf.definition,     "Definition")
+          k("n", "gr", vim.lsp.buf.references,     "References")
+          k("n", "gi", vim.lsp.buf.implementation, "Implementation")
+          k("n", "K",  vim.lsp.buf.hover,          "Hover")
+          k("n", "<leader>cr", vim.lsp.buf.rename,      "Rename")
+          k("n", "<leader>ca", vim.lsp.buf.code_action, "Code action")
+          k("n", "<leader>cf", function() vim.lsp.buf.format({ async = true }) end, "Format")
+          k("n", "]d", function() vim.diagnostic.jump({ count =  1, float = true }) end, "Next diagnostic")
+          k("n", "[d", function() vim.diagnostic.jump({ count = -1, float = true }) end, "Prev diagnostic")
+        end,
+      })
 
       vim.diagnostic.config({
-        virtual_text   = { prefix = "●" },
-        severity_sort  = true,
-        float          = { border = "rounded" },
+        virtual_text  = { prefix = "●" },
+        severity_sort = true,
+        float         = { border = "rounded" },
       })
     end,
   },
